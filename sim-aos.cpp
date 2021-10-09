8d91310f1a1a4e58aa01c17a2000e03570ea10c9
Ivan <100392936@alumnos.uc3m.es>
2021-10-09 17:59:16 +0200

    1

---
+++ sim-aos.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ivan on 9/10/21.
+//
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char  ) {
+    struct Object{
+        dobule px,py,pz, vx, vy, vz;
+        int m;
+    };
+    Object universe [] = object
+
+
+}
