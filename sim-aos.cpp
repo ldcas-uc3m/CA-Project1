fd5b7034d4250178451fde0727dedb76563e274c
ldcas-uc3m <100429021@alumnos.uc3m.es>
2021-10-12 17:30:30 +0200

    fixed parameters

--- sim-aos.cpp
+++ sim-aos.cpp
@@ -68,94 +68,93 @@ auto parametersGeneration(int argc, const char * argcv[]){
     return universe;
 }
 
- void objectCollision(Object * a, Object * b){
-    // TODO: Ivan
-    a->m = a->m +b->m;
-    a->vx = a->vx + b->vx;
-    a->vy = a->vy + b->vy;
-    a->vz = a->vz + b->vz;
-    delete b;
+ void objectCollision(Object a, Object b){
+    a.m = a.m +b.m;
+    a.vx = a.vx + b.vx;
+    a.vy = a.vy + b.vy;
+    a.vz = a.vz + b.vz;
+    delete &b;
     return;
 }
 
 
-void updatePosition(Object * obj, int num){
+void updatePosition(Object obj, int num){
     double a[3] = {0};
     // acceleration calculation
     for(int k = 0; k <3; k++){
         for(int l = 0; l < num_objects; l++){
             a[k] += forces[3*num + k][3*l + k];
-            //a[k] += obj->forces[3*l + k];
+            //a[k] += obj.forces[3*l + k];
         }
-        a[k] /= obj->m;
+        a[k] /= obj.m;
     }
 
     // velocity calculation
-    double vx = obj->vx + a[0] * time_step;
-    double vy = obj->vy + a[1] * time_step;
-    double vz = obj->vz + a[2] * time_step;
+    double vx = obj.vx + a[0] * time_step;
+    double vy = obj.vy + a[1] * time_step;
+    double vz = obj.vz + a[2] * time_step;
 
-    obj->vx = vx;
-    obj->vy = vy;
-    obj->vz = vz;
+    obj.vx = vx;
+    obj.vy = vy;
+    obj.vz = vz;
 
     // position calculation
-    obj->px += vx * time_step;
-    obj->py += vz * time_step;
-    obj->py += vz * time_step;
+    obj.px += vx * time_step;
+    obj.py += vz * time_step;
+    obj.py += vz * time_step;
 
     // rebound effect
 
     // p <= 0
-    if(obj->px <= 0){
-        obj->px = 0;
-        obj->vx = - obj->vx;
+    if(obj.px <= 0){
+        obj.px = 0;
+        obj.vx = - obj.vx;
     }
 
-    if(obj->py <= 0){
-        obj->py = 0;
-        obj->vy = - obj->vy;
+    if(obj.py <= 0){
+        obj.py = 0;
+        obj.vy = - obj.vy;
     }
 
-    if(obj->pz <= 0){
-        obj->pz = 0;
-        obj->vz = - obj->vz;
+    if(obj.pz <= 0){
+        obj.pz = 0;
+        obj.vz = - obj.vz;
     }
 
     // p >= size_enclosure
-    if(obj->px >= size_enclosure){
-        obj->px = size_enclosure;
-        obj->vx = - obj->vx;
+    if(obj.px >= size_enclosure){
+        obj.px = size_enclosure;
+        obj.vx = - obj.vx;
     }
 
-    if(obj->py >= size_enclosure){
-        obj->py = size_enclosure;
-        obj->vy = - obj->vy;
+    if(obj.py >= size_enclosure){
+        obj.py = size_enclosure;
+        obj.vy = - obj.vy;
     }
 
-    if(obj->pz >= size_enclosure){
-        obj->pz = size_enclosure;
-        obj->vz = - obj->vz;
+    if(obj.pz >= size_enclosure){
+        obj.pz = size_enclosure;
+        obj.vz = - obj.vz;
     }
-
+    
     return;
 }
 
 
-double * forceComputation(Object * a, Object * b){
-    if((a->px == b->px) && (a->py == b->py) && (a->pz == b->pz)){
+double * forceComputation(Object a, Object b){
+    if((a.px == b.px) && (a.py == b.py) && (a.pz == b.pz)){
         // TODO: cambiar esto, es posible que con el timestep no coincidan exactamente
         objectCollision(a, b);
         return 0;  // TODO: cambiar esta mierda a vectores y eliminar el vector en forces
     }
-    double dx = b->px - a->px;
-    double dy = b->py - a->py;
-    double dz = b->pz - a->pz;
+    double dx = b.px - a.px;
+    double dy = b.py - a.py;
+    double dz = b.pz - a.pz;
     
     double f[3];
-    f[0] = (g * a->m * b->m * dx) / abs(pow(dx, 3));
-    f[1] = (g * a->m * b->m * dy) / abs(pow(dy, 3));
-    f[2] = (g * a->m * b->m * dz) / abs(pow(dz, 3));
+    f[0] = (g * a.m * b.m * dx) / abs(pow(dx, 3));
+    f[1] = (g * a.m * b.m * dy) / abs(pow(dy, 3));
+    f[2] = (g * a.m * b.m * dz) / abs(pow(dz, 3));
 
     return f;
 }
@@ -177,8 +176,8 @@ int main(int argc, const char * argcv[]){
         }
         for(int i = 0; i < num_objects; i++){
             for(int j = ++i; j < num_objects; j++){
-                Object * a = universe[i];
-                Object * b = universe[j];
+                Object a = universe[i];
+                Object b = universe[j];
                 double * result = forceComputation(a, b);
                 forces[3*i][3*j] = result[0];
                 forces[3*j][3*i] = - result[0];
