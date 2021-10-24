1a4343f93320c823ed1e5991690e967b6558cfb0
ldcas-uc3m <100429021@alumnos.uc3m.es>
2021-10-24 09:13:27 +0200

    delete universe

--- sim-soa.cpp
+++ sim-soa.cpp
@@ -52,6 +52,18 @@ class Universe{
         double * ax;
         double * ay;
         double * az;
+
+        ~Universe(){
+            free(px);
+            free(py);
+            free(pz);
+            free(vx);
+            free(vy);
+            free(vy);
+            free(ax);
+            free(ay);
+            free(az);
+        }
 };
 
 
@@ -254,4 +266,6 @@ int main(int argc, const char ** argcv){
     }
 
     OutFile.close();
+
+    delete(&universe);
 }
