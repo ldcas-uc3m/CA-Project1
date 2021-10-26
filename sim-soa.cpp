5b4db9576737f88d19aafb8271b5059b76b60911
L. Daniel Casais <78721925+ldcas-uc3m@users.noreply.github.com>
2021-10-26 14:05:45 +0200

    El humano es retrasado y no tiene cura Parte L
    La L es de "Luisda"

--- sim-soa.cpp
+++ sim-soa.cpp
@@ -234,7 +234,7 @@ int main(int argc, const char ** argcv){
                 double dz = universe.pz[j] - universe.pz[i];
                 double distance = sqrt(dx*dx + dy*dy + dz*dz);
 
-                if((dx <= COL_DISTANCE) || (dy <= COL_DISTANCE) || (dz <= COL_DISTANCE)){
+                if((dx <= COL_DISTANCE) && (dy <= COL_DISTANCE) && (dz <= COL_DISTANCE)){
                     /* ---
                     OBJECT COLLISION
                     --- */
@@ -339,5 +339,5 @@ int main(int argc, const char ** argcv){
 
     OutFile.close();
 
-    delete(&universe);
+    //delete(&universe);
 }
